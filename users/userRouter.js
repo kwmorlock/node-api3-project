889e4a0fbe689a8282cfa0be818a3129b8f14c29
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-05-06 19:18:39 -0400

    put updated in userrouter

--- users/userRouter.js
+++ users/userRouter.js
@@ -98,6 +98,21 @@ router.delete('/:id', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // do your magic!
+  const changes = req.body;
+  Udb.update(req.params.id, changes)
+    .then(udb => {
+      if (udb) {
+        res.status(200).json(udb);
+      } else {
+        res.status(404).json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+          error: "The post information could not be modified.",
+      });
+    });
 });
 
 //custom middleware
