b90088ade7536670b0e7bdd97fd5a42443226160
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-05-06 15:19:13 -0400

    index updated

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 // code away!
 //npm i and npm install express
+
+const server = require('./server.js');
+
+server.listen(4000, () => {
+  console.log('\n* Server Running on http://localhost:4000 *\n');
+});
