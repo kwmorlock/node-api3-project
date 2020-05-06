9e1c80e6baea4187e57e6adde46968fe5b768a5b
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-05-06 17:17:41 -0400

    gets setup

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -4,12 +4,36 @@ const Db = require('./postDb.js');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get("/api/posts", (req, res) => {
   // do your magic!
+  Db.find(req.query)
+  .then(db => {
+    res.status(200).json(db);
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).json({
+      error: "The posts information could not be retrieved.",
+    });
+  });
 });
 
-router.get('/:id', (req, res) => {
+router.get("/api/users/:id", (req, res) => {
   // do your magic!
+  Db.getById(req.params.id)
+  .then(db => {
+    if (db) {
+      res.status(200).json(db);
+    } else {
+      res.status(404).json({ message: "The post with the specified ID does not exist." });
+    }
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).json({
+      message: "Error retrieving the db",
+    });
+  });
 });
 
 router.delete('/:id', (req, res) => {
